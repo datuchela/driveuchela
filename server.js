038614753b95afa5ef9f9a79bfa41f881f63cf1d
Davit Narimanidze <davit.narimanidze408@ens.tsu.edu.ge>
2022-07-01 01:43:56 +0400

    testing clipboard hello

--- server.js
+++ server.js
@@ -22,6 +22,26 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+const uploadMiddleware = async (req, res) => {
+  let loading = true
+  upload.single('file')
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  }
+  if (req.body.password !== null && req.body.password !== '') {
+    fileData.password = await bcrypt.hash(req.body.password, 10)
+  }
+  const file = await File.create(fileData)
+  loading = false
+  res.render('index', {
+    fileLink: `${req.headers.origin}/file/${file.id}`,
+    fileName: file.originalName,
+    password: req.body.password,
+    loading: loading,
+  })
+}
+
 app.post('/upload', upload.single('file'), async (req, res) => {
   const fileData = {
     path: req.file.path,
@@ -31,7 +51,12 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     fileData.password = await bcrypt.hash(req.body.password, 10)
   }
   const file = await File.create(fileData)
-  res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}`, fileName: file.originalName, password: req.body.password })
+  loading = false
+  res.render('index', {
+    fileLink: `${req.headers.origin}/file/${file.id}`,
+    fileName: file.originalName,
+    password: req.body.password,
+  })
 })
 
 const handleDownload = async (req, res) => {
